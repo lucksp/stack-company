cb73a87f9526dc5903540c3e39b60271b2ccf349
Phil Lucks <philip.lucks@snapengage.com>
2019-03-26 19:47:15 -0600

    Update fieldsSchema.js

--- src/scripts/helpers/fieldsSchema.js
+++ src/scripts/helpers/fieldsSchema.js
@@ -2,9 +2,12 @@ import * as Yup from "yup";
 
 export const NewCompanySchema = Yup.object().shape({
   name: Yup.string().required("Required"),
-  address: Yup.string().required("Required")
-  // revenue: Yup.number().required("Required"),
-  // phone: Yup.number().required("Required")
+  address: Yup.string().required("Required"),
+  revenue: Yup.number().required("Required"),
+  phone: Yup.string()
+    .min(2, "Too Short!")
+    .max(10, "Too Long!")
+    .required("Required")
 });
 
 export const NewEmployyeSchema = Yup.object().shape({
